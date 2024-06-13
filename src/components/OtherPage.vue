8540a5704d9e685897946f995b8356169d131378
y-ma3 <yuma33t@gmail.com>
2024-06-13 13:29:21 +0900

    refactor: コードのフォーマット整形

--- src/components/OtherPage.vue
+++ src/components/OtherPage.vue
@@ -3,93 +3,72 @@
   <v-app>
     <v-app-bar flat>
       <v-container class="mx-auto">
-      <p class="text-h5" style="font-weight: bold" >学生面談（工学部）~面談選択画面~</p>
-    </v-container>
-  </v-app-bar>
-  <v-main class="bg-grey-lighten-3">
-  <v-card max-width="600" class="mx-auto">
-    <p class="text-h5" style="font-weight: bold">{{ text }}さん ようこそ</p>
-    
-    <p class="text-h7" style="font-weight: bold">{{ items }}</p>
+        <p class="text-h5" style="font-weight: bold">学生面談（工学部）~面談選択画面~</p>
+      </v-container>
+    </v-app-bar>
+    <v-main class="bg-grey-lighten-3">
+      <v-card max-width="600" class="mx-auto">
+        <p class="text-h5" style="font-weight: bold">{{ text }}さん ようこそ</p>
 
-    <v-container >
-    <v-col cols="12">
-          <v-card color="#f0f8ff" theme="dark">
-            <div class="d-flex flex-no-wrap justify-space-between">
-              <div>
+        <p class="text-h7" style="font-weight: bold">{{ items }}</p>
+
+        <v-container>
+          <v-col cols="12">
+            <v-card color="#f0f8ff" theme="dark">
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <div>
 
 
-                <v-card-title class="text-h5"> 学業について </v-card-title>
+                  <v-card-title class="text-h5"> 学業について </v-card-title>
 
-                <v-card-subtitle>成績,出席率,好きな講義等...</v-card-subtitle>
+                  <v-card-subtitle>成績,出席率,好きな講義等...</v-card-subtitle>
 
-              </div>
-              <v-card-actions>
-                <v-btn
-                  class="ma-2"
-                  variant="outlined"
-                  width="75" height="75"
-                  color="white"
-                  style="font-weight: bold;background-color: #4169e1;"
-                  @click="() => $router.push({ name: 'interview1',query: {name: text }})"
-                
-                  >面談に<br>進む
-                  
-                  
-                  <v-icon
-                    size="large"
-                    icon="mdi-arrow-right-bold-box-outline"
-                  ></v-icon>
-                </v-btn>
+                </div>
+                <v-card-actions>
+                  <v-btn class="ma-2" variant="outlined" width="75" height="75" color="white"
+                    style="font-weight: bold;background-color: #4169e1;"
+                    @click="() => $router.push({ name: 'interview1', query: { name: text } })">面談に<br>進む
+
+
+                    <v-icon size="large" icon="mdi-arrow-right-bold-box-outline"></v-icon>
+                  </v-btn>
                 </v-card-actions>
-            </div>
-          </v-card>
-        </v-col>
-
-
-        <v-col cols="12">
-          <v-card color="#f0f8ff" theme="dark">
-            <div class="d-flex flex-no-wrap justify-space-between">
-              <div>
-                <v-card-title class="text-h5"> 学生生活について </v-card-title>
-
-                <v-card-subtitle>サークル,アルバイト等...</v-card-subtitle>
-
               </div>
-              <v-card-actions>
-                <v-btn
-                  class="ma-2"
-                  variant="outlined"
-                  width="75" height="75"
-                  color="white"
-                  style="font-weight: bold;background-color: #4169e1;"
-                  @click="() => $router.push({ name: 'interview2',query: {name: text }})"
-                >
-                  面談に<br>進む
-                  
-                  
-                  <v-icon
-                    size="large"
-                    icon="mdi-arrow-right-bold-box-outline"
-                  ></v-icon>
-                </v-btn>
-                </v-card-actions>
-            </div>
-          </v-card>
-        </v-col>
+            </v-card>
+          </v-col>
 
-        <v-btn
-          color="success"
-          class="mt-4"
-          block
-          @click ="() => $router.push({ name: 'finish',query: {name: text}})"
-          >面談終了
-        </v-btn>
-        
-  </v-container>
-  </v-card>
-</v-main>
-</v-app>
+
+          <v-col cols="12">
+            <v-card color="#f0f8ff" theme="dark">
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <div>
+                  <v-card-title class="text-h5"> 学生生活について </v-card-title>
+
+                  <v-card-subtitle>サークル,アルバイト等...</v-card-subtitle>
+
+                </div>
+                <v-card-actions>
+                  <v-btn class="ma-2" variant="outlined" width="75" height="75" color="white"
+                    style="font-weight: bold;background-color: #4169e1;"
+                    @click="() => $router.push({ name: 'interview2', query: { name: text } })">
+                    面談に<br>進む
+
+
+                    <v-icon size="large" icon="mdi-arrow-right-bold-box-outline"></v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </div>
+            </v-card>
+          </v-col>
+
+          <v-btn color="success" class="mt-4" block
+            @click="() => $router.push({ name: 'finish', query: { name: text } })">面談終了
+          </v-btn>
+
+        </v-container>
+      </v-card>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -98,37 +77,37 @@ export default {
   name: 'OtherPage',
   data() {
     //ここで引数を受け取る
-    return{
-    id: '',
-    text: '',
-    q1:[],
-    int1:[],
-    t1:'',
-    ans:'',
+    return {
+      id: '',
+      text: '',
+      q1: [],
+      int1: [],
+      t1: '',
+      ans: '',
 
-    items:""
+      items: ""
     }
   },
-  created(){
-    if(this.$route.query.name){
-    this.text = this.$route.query.name;
+  created() {
+    if (this.$route.query.name) {
+      this.text = this.$route.query.name;
     }
-    if(this.$route.query.ans){
+    if (this.$route.query.ans) {
       this.ans1 = JSON.parse(JSON.stringify(this.$route.query.ans))
     }
   },
-  methods:{
-    Submit(){
-      
+  methods: {
+    Submit() {
+
     },
-    postData(){
+    postData() {
       db.collection('students').doc("Udyu2MqnkkrNIgsMxFEh").update({
-              ans1: String(this.ans),
-          }).then((response) => {
-              console.log(response);            
-            }).catch((error) => {
-              console.log(error);
-          });
+        ans1: String(this.ans),
+      }).then((response) => {
+        console.log(response);
+      }).catch((error) => {
+        console.log(error);
+      });
     },
 
     getData: function () {
@@ -136,18 +115,18 @@ export default {
       // データ取得
       db.collection('students').where('name', '==', String(this.text)).get(
       ).then((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
-            //this.data = doc.data()
-              // テーブル表示
-              this.items = doc.data().num2;
-              console.log(this.items)
-          });
+        querySnapshot.forEach((doc) => {
+          //this.data = doc.data()
+          // テーブル表示
+          this.items = doc.data().num2;
+          console.log(this.items)
+        });
       }).catch((error) => {
-          console.log(error);
-          // テーブルリセット
-          this.items = [];
+        console.log(error);
+        // テーブルリセット
+        this.items = [];
       });
-      },
+    },
   }
 }
 </script>
--- src/router.js
+++ src/router.js
@@ -8,33 +8,32 @@ import Finish from './components/Finish.vue'
 
 
 export const router = createRouter({
-    history: createWebHistory('/'),
-    routes: [
-      {
-        path: '/',
-        name: 'main',
-        component: MainPage,
-      },
-      {
-        path: '/other', //pathの書き方を変更
-        name: 'other',
-        component: OtherPage,
-      },
-      {
-        path: '/interview1', //pathの書き方を変更
-        name: 'interview1',
-        component: Interview1,
-      },
-      {
-        path: '/interview2', //pathの書き方を変更
-        name: 'interview2',
-        component: Interview2,
-      },
-      {
-        path: '/finish', //pathの書き方を変更
-        name: 'finish',
-        component: Finish,
-      }
-    ]
-  })
-  
+  history: createWebHistory('/'),
+  routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: MainPage,
+    },
+    {
+      path: '/other', //pathの書き方を変更
+      name: 'other',
+      component: OtherPage,
+    },
+    {
+      path: '/interview1', //pathの書き方を変更
+      name: 'interview1',
+      component: Interview1,
+    },
+    {
+      path: '/interview2', //pathの書き方を変更
+      name: 'interview2',
+      component: Interview2,
+    },
+    {
+      path: '/finish', //pathの書き方を変更
+      name: 'finish',
+      component: Finish,
+    }
+  ]
+})
