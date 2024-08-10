54c1d763b660f3475713ec9c735a6ccdfa85d5ef
yuma.tsuri <yuma33t@gmail.com>
2024-08-10 13:13:41 +0900

    アバター選択画面を実装

--- client/lib/view/start/start_view.dart
+++ client/lib/view/start/start_view.dart
@@ -1,6 +1,7 @@
 import 'package:client/component/input_text_field.dart';
 import 'package:client/constant/color.dart';
 import 'package:client/constant/select_items.dart';
+import 'package:client/view/avatar_select/avatar_select_view.dart';
 import 'package:client/view_model/start/start_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -27,75 +28,82 @@ class _StartView extends ConsumerState<StartView> {
   Widget build(BuildContext context) {
     final viewModel = ref.watch(startViewModelProvider.notifier);
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text("学生面談(工学部)"),
-        ),
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.only(top: 20),
-            child: Form(
-              key: formKey,
-              child: Column(
-                children: [
-                  _buildSelectMajorPullDown(viewModel),
-                  const SizedBox(height: 15),
-                  buildInputTextField(
-                    viewModel,
-                    labelText: "学籍番号",
-                    hintText: "1234567",
-                    limitText: 7,
-                    validator: (value) {
-                      if (!RegExp(r'^[0-9-]+$').hasMatch(value)) {
-                        return '数字のみを入力してください';
-                      } else if (value.length != 7) {
-                        return '7桁で入力してください';
-                      }
-                    },
-                    onChanged: (value) {
-                      viewModel.setSchoolNumber(value);
-                    },
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("学生面談(工学部)"),
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.only(top: 20),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                _buildSelectMajorPullDown(viewModel),
+                const SizedBox(height: 15),
+                buildInputTextField(
+                  viewModel,
+                  labelText: "学籍番号",
+                  hintText: "1234567",
+                  limitText: 7,
+                  validator: (value) {
+                    if (!RegExp(r'^[0-9-]+$').hasMatch(value)) {
+                      return '数字のみを入力してください';
+                    } else if (value.length != 7) {
+                      return '7桁で入力してください';
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    viewModel.setSchoolNumber(value);
+                  },
+                ),
+                const SizedBox(height: 15),
+                buildInputTextField(
+                  viewModel,
+                  labelText: "クラスー名列番号",
+                  hintText: "1EP1-01",
+                  onChanged: (value) {
+                    viewModel.setClassColumnNumber(value);
+                  },
+                ),
+                const SizedBox(height: 15),
+                buildInputTextField(
+                  viewModel,
+                  labelText: "氏名",
+                  hintText: "金工 太郎",
+                  validator: (value) {
+                    // ひらがな、カタカナ、漢字、アルファベット、"ー"、"・"のみ
+                    if (!RegExp(r'^[ぁ-んァ-ン一-龠a-zA-Zー・]+$').hasMatch(value)) {
+                      return '使用できない文字が含まれています。';
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    viewModel.setName(value);
+                  },
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState?.validate() ?? false) {
+                      // バリデーションが成功した場合にのみ画面遷移を行う
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => const AvatarSelectView(),
+                        ),
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    minimumSize: const Size(100, 50),
                   ),
-                  const SizedBox(height: 15),
-                  buildInputTextField(
-                    viewModel,
-                    labelText: "クラスー名列番号",
-                    hintText: "1EP1-01",
-                    onChanged: (value) {
-                      viewModel.setClassColumnNumber(value);
-                    },
-                  ),
-                  const SizedBox(height: 15),
-                  buildInputTextField(
-                    viewModel,
-                    labelText: "氏名",
-                    hintText: "金工 太郎",
-                    validator: (value) {
-                      // ひらがな、カタカナ、漢字、アルファベット、"ー"、"・"のみ
-                      if (!RegExp(r'^[ぁ-んァ-ン一-龠a-zA-Zー・]+$').hasMatch(value)) {
-                        return '使用できない文字が含まれています。';
-                      }
-                    },
-                    onChanged: (value) {
-                      viewModel.setName(value);
-                    },
-                  ),
-                  const SizedBox(height: 30),
-                  ElevatedButton(
-                    onPressed: () {
-                      formKey.currentState?.validate();
-                    },
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Theme.of(context).colorScheme.onPrimary,
-                      backgroundColor: Theme.of(context).colorScheme.primary,
-                      minimumSize: const Size(100, 50),
-                    ),
-                    child: const Text("次へ"),
-                  )
-                ],
-              ),
+                  child: const Text("次へ"),
+                )
+              ],
             ),
           ),
         ),
