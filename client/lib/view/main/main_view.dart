828fe41736b77fa88edaa87be3252e32e56e9b96
yuma.tsuri <yuma.tsuri@optim.co.jp>
2024-08-18 16:11:44 +0900

    intlを導入

---
+++ client/lib/view/main/main_view.dart
@@ -0,0 +1,44 @@
+import 'dart:ui';
+
+import 'package:client/generated/l10n.dart';
+import 'package:client/view/profile_input/profile_input_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// 画面全体のウィジェット
+class MainView extends ConsumerStatefulWidget {
+  const MainView({super.key});
+
+  @override
+  ConsumerState<MainView> createState() => _MainView();
+}
+
+class _MainView extends ConsumerState<MainView> {
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      scrollBehavior: CustomScrollBehavior(),
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      home: const ProfileInputView(),
+    );
+  }
+}
+
+// スクロールの挙動をカスタマイズするためのクラス
+class CustomScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        // Webでのスクロールをマウスで行うために追加
+        PointerDeviceKind.mouse,
+      };
+}
