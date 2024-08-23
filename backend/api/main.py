4a0e06194fdfb18f8f5bec08a2de4f8e1344cbfe
fujisaki <fujidan8464@gmail.com>
2024-08-23 23:41:21 +0900

    Docker化 & postgres データベース動くようにしました

--- backend/api/main.py
+++ backend/api/main.py
@@ -5,4 +5,8 @@ app = FastAPI()
 
 @app.get("/hello")
 async def hello():
-    return {"message": "hello world!"}
+    return {"message": "hello world!あ"}
+
+
+# @app.get("/data")
+# async def data():
