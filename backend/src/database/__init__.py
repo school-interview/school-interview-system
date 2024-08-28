57d55543048d14d4274c938e8dfc9086c66298f2
fujisaki <fujidan8464@gmail.com>
2024-08-28 19:21:45 +0900

    とりあえずpostgressとの連携完了

--- backend/src/database/__init__.py
+++ backend/src/database/__init__.py
@@ -1,8 +1,18 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from models import ModelBase
+
+from models import User
 
 
 async def connect_db():
     engine = create_engine(
-        "postgresql://user:postgres@db:5432/school-interview")
-    async with engine.connect() as conn:
-        return conn
+        "postgresql://user:postgres@db:5432/school-interview", echo=True)
+    connection = engine.connect()
+    with Session(engine) as session:
+        ModelBase.metadata.create_all(engine)
+        user = User(name="dan", studentId="1119059",
+                    department="情報工学科", grade=4)
+        session.add(user)
+        session.commit()
+    return connection
--- backend/src/models/user_model.py
+++ backend/src/models/user_model.py
@@ -1,10 +1,12 @@
-from base import ModelBase
+from models.base_model import ModelBase
 from sqlalchemy import String
 from sqlalchemy.orm import mapped_column, Mapped
 
 
 class User(ModelBase):
-    __tablename__ = "users"
+    __tablename__ = "Users"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
-    studentId: Mapped[str] =
+    studentId: Mapped[str] = mapped_column(String(7))
+    department: Mapped[str] = mapped_column(String(30))
+    grade: Mapped[int]
