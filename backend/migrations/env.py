ff52b1e456e27e89183a2eab656423adcb0c6a2b
fujisaki <fujidan8464@gmail.com>
2024-08-30 14:47:53 +0900

    コンテナ起動時にAlembicを用いて自動でマイグレーションするようにしまいた

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -7,9 +7,17 @@ from alembic import context
 
 from src.models.base_model import BaseModel
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
