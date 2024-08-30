39e1b33a1533be858e5d99cac706efc89c3ce55c
fujisaki <fujidan8464@gmail.com>
2024-08-30 14:47:53 +0900

    コンテナ起動時にAlembicを用いて自動でマイグレーションするようにしまいた

--- backend/launch-server.sh
+++ backend/launch-server.sh
@@ -1,2 +1,5 @@
 #!/bin/ash
+echo "Start migrating database..."
+poetry run alembic upgrade head
+echo "Database migrated successfully"
 poetry run uvicorn src.main:app_socketio --reload --host 0.0.0.0 --port 8000
--- backend/src/database/__init__.py
+++ backend/src/database/__init__.py
@@ -1,8 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from models import BaseModel
-
-from models import User
 
 
 async def connect_db():
@@ -10,6 +7,5 @@ async def connect_db():
         "postgresql://user:postgres@db:5432/school-interview", echo=True)
     connection = engine.connect()
     session = Session(engine)
-    BaseModel.metadata.create_all(engine)
     session.commit()
     return engine, connection, session
