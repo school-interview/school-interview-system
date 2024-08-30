cfce382c7d42e2787e86b5cb4539489cec4e0f25
fujisaki <fujidan8464@gmail.com>
2024-08-31 02:04:03 +0900

    speack_to_teacherイベントを登録

--- backend/src/controllers/__init__.py
+++ backend/src/controllers/__init__.py
@@ -1,6 +1,9 @@
 from typing import List
 from src.controllers.rest_api.auth_controller import auth_controllers
 from src.models import RestApiController, HttpMethod, LoginRequest
+from src.controllers.websocket.interview_controller import websocket_controllers
+from src.models.websocket_controller_model import WebsocketController
+
 
 rest_api_controllers: List[RestApiController] = [*auth_controllers]
-websocket_controllers: List = []
+websocket_controllers: List[WebsocketController] = [*websocket_controllers]
