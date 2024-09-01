de16e9b434bf3c3c7b00c8aebe173bdf26ec00c7
fujisaki <fujidan8464@gmail.com>
2024-09-01 19:09:01 +0900

    固定の返答しか返ってこないけれど話しかける機能追加

--- backend/src/controllers/__init__.py
+++ backend/src/controllers/__init__.py
@@ -1,9 +1,8 @@
 from typing import List
 from src.controllers.rest_api.auth_controller import auth_controllers
-from src.models import RestApiController, HttpMethod, LoginRequest
+from src.models import RestApiController, WebsocketController
 from src.controllers.websocket.interview_controller import interview_websocket_controllers
 from src.controllers.websocket.connection_controller import connection_websocket_controllers
-from src.models.websocket_controller_model import WebsocketController
 
 
 rest_api_controllers: List[RestApiController] = [*auth_controllers]
