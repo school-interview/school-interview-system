319f58d8189941968c819cd440222f55bdc9f271
fujisaki <fujidan8464@gmail.com>
2024-09-01 19:09:01 +0900

    固定の返答しか返ってこないけれど話しかける機能追加

--- backend/src/controllers/websocket/connection_controller.py
+++ backend/src/controllers/websocket/connection_controller.py
@@ -1,12 +1,13 @@
 from fastapi import Depends
+from src.usecases.websocket_connection.connection_managemet import delete_connection, register_connection
 from src.models.db_models.user_model import User
 from src.database import SessionMaker
 from src.models.websocket_controller_model import WebsocketController
 from typing import Dict, List
-from src.usecases.websocket_connection.connection_managemet import register_connection
 from src.usecases.auth.authenticate import authenticate
 from sqlalchemy.orm import sessionmaker
 import logging
+from websocket_server import sio
 
 
 async def connect(sid: str, environ, auth: Dict):
@@ -15,14 +16,27 @@ async def connect(sid: str, environ, auth: Dict):
     session = SessionMaker()
     try:
         user = authenticate(session, auth['user_id'])
-        logging.info("認証成功: user_id:{}".format(user.id))
+        register_connection(session, user.id, sid)
+        socket_io_session = {
+            'user': user
+        }
+        sio.save_session(sid, socket_io_session)
+        logging.info("Authentication Succeed : user_id:{}".format(user.id))
     except Exception as e:
+        logging.error("sid:{}  failed to connect".format(sid))
         raise ConnectionRefusedError('authentication failed')
     finally:
         session.close()
 
 
 async def disconnect(sid: str):
+    session = SessionMaker()
+    try:
+        delete_connection(session, sid)
+    except Exception as e:
+        logging.error("sid:{}  failed to disconnect".format(sid))
+    finally:
+        session.close()
     logging.info("sid:{}  disconnected".format(sid))
 
 
--- backend/src/models/db_models/connection_model.py
+++ backend/src/models/db_models/connection_model.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 class Connection(BaseModel):
     __tablename__ = "Connections"
+    __table_args__ = {'extend_existing': True}
     id: Mapped[UUID] = mapped_column(primary_key=True)
     socket_id: Mapped[str] = mapped_column(String(100))
     user_id: Mapped[UUID] = mapped_column(ForeignKey("Users.id"))
