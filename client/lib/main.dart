e203ddc4a35f175eafda87dc417e4474195cd363
yuma.tsuri <yuma33t@gmail.com>
2024-09-05 13:23:08 +0900

    43
    device_previewを導入

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:client/view/main/main_view.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(
-    const ProviderScope(
-      child: MainView(),
-    ),
-  );
+  const app = MainView();
+  const scope = ProviderScope(child: app);
+  final devicePreview = DevicePreview(builder: (_) => scope);
+  runApp(devicePreview);
 }
