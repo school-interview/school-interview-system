be4ad0c21ea43cf32d47d74714a92994a5cf7a9c
fujisaki <fujidan8464@gmail.com>
2024-09-18 21:11:30 +0900

    speak_to_teacherがchatgptに対応

--- backend/src/usecases/interview/speak_to_teacher.py
+++ backend/src/usecases/interview/speak_to_teacher.py
@@ -28,8 +28,34 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 # 会話の履歴を保持するためのdict（一旦メモリ上に保持）
 chat_history_store = {}
 
-
+# 質問たちはそんなにないので、一度取得したらキャッシュしておく
 questions: Dict[int, InterviewQuestion] = {}
+
+# ローカルLLMで動かすLlamaのプロンプトテンプレート
+speak_to_teacher_llama_prompt_template = """<s>[INST] <<SYS>>
+あなたは学生の就学アドバイザーです。学生の悩みに対して与えられた情報に基づいて、適切なアドバイスを提供してください。必ず学生の悩みに答えるような回答を心がけてください。
+<</SYS>>
+{context}
+{history}
+[/INST]
+{question}
+</s>"""
+
+# ChatGPTのプロンプトテンプレート
+speak_to_teacher_chat_gpt_prompt_template = """
+あなたは学生の就学アドバイザーです。学生の悩みに対して与えられた情報に基づいて、適切なアドバイスを提供してください。必ず学生の悩みに答えるような回答を心がけてください。
+
+次のデータをもとに、学生にアドバイスをしても良いです。
+{context}
+
+
+これはこれまでの会話履歴です。
+{history}
+
+
+
+学生の発言：{question}
+"""
 
 
 def getQuestions(session: Session):
@@ -47,21 +73,18 @@ def speak_to_teacher(session: Session, interview_session: InterviewSessionModel,
     use_local_llm = bool(os.getenv("USE_LOCAL_LLM"))
     if interview_session.done:
         raise Exception("The interview session is already done.")
-    questions = getQuestions(session)
-    extraction_result = extract_answer(
-        interview_session, message_from_user, questions)
-    # if not extraction_result.succeeded_to_extract:
-
-    # else:
-    interview_session.progress += 1
-    session.commit()
+    # questions = getQuestions(session)
+    # extraction_result = extract_answer(
+    #     interview_session, message_from_user, questions)
+    # interview_session.progress += 1
+    # session.commit()
     response_from_teacher = generate_message_from_teacher(
-        interview_session, message_from_user)
+        interview_session, message_from_user, use_local_llm)
 
     return TeacherResponse(message_from_teacher=response_from_teacher)
 
 
-def generate_message_from_teacher(interview_session: InterviewSessionModel, message_from_student: str):
+def generate_message_from_teacher(interview_session: InterviewSessionModel, message_from_student: str, use_local_llm: bool = False):
     CONTEXT_SIZE = 4096
     LLM_FILE = "src/llm/ELYZA-japanese-Llama-2-7b-instruct-q2_K.gguf"
     CHUNK_SIZE = 256
@@ -83,32 +106,6 @@ def generate_message_from_teacher(interview_session: InterviewSessionModel, mess
 
     def format_docs(docs):
         return "\n\n".join(doc.page_content for doc in docs)
-    llm = ChatLlamaCpp(
-        model_path=LLM_FILE,
-        n_gpu_layers=128,
-        n_ctx=CONTEXT_SIZE,
-        f16_kv=True,
-        verbose=True,
-        seed=0
-    )
-    template = """<s>[INST] <<SYS>>
-    あなたは学生の就学アドバイザーです。学生の悩みに対して与えられた情報に基づいて、適切なアドバイスを提供してください。必ず学生の悩みに答えるような回答を心がけてください。
-    <</SYS>>
-    {context}
-    {history}
-    [/INST]
-    {question}
-    </s>"""
-    prompt = ChatPromptTemplate.from_template(template)
-    # {"question": RunnablePassthrough(), "context": retriever | format_docs}
-    rag_chain = (
-        {"context": RunnableLambda(lambda x: x['question']) | retriever | format_docs,
-         "question": RunnablePassthrough(),
-         "history": RunnableLambda(lambda x: x['history'])}
-        | prompt
-        | llm
-        | StrOutputParser()
-    )
 
     def get_session_history(session_id: str) -> BaseChatMessageHistory:
         if session_id not in chat_history_store:
@@ -116,17 +113,62 @@ def generate_message_from_teacher(interview_session: InterviewSessionModel, mess
         print("history_store", chat_history_store[session_id])
         return chat_history_store[session_id]
 
-    conversational_rag_chain = RunnableWithMessageHistory(
-        rag_chain,
-        get_session_history,
-        input_messages_key="question",
-        history_messages_key="history",
-    )
+    if use_local_llm:
+        llm = ChatLlamaCpp(
+            model_path=LLM_FILE,
+            n_gpu_layers=128,
+            n_ctx=CONTEXT_SIZE,
+            f16_kv=True,
+            verbose=True,
+            seed=0
+        )
 
-    response = conversational_rag_chain.invoke({"question": message_from_student}, config={
-        "configurable": {"session_id": interview_session.id.__str__()}})
-    vectorstore.delete_collection()
-    return response
+        prompt = ChatPromptTemplate.from_template(
+            speak_to_teacher_llama_prompt_template)
+        rag_chain = (
+            {"context": RunnableLambda(lambda x: x['question']) | retriever | format_docs,
+             "question": RunnablePassthrough(),
+             "history": RunnableLambda(lambda x: x['history'])}
+            | prompt
+            | llm
+            | StrOutputParser()
+        )
+        conversational_rag_chain = RunnableWithMessageHistory(
+            rag_chain,
+            get_session_history,
+            input_messages_key="question",
+            history_messages_key="history",
+        )
+        response = conversational_rag_chain.invoke({"question": message_from_student}, config={
+            "configurable": {"session_id": interview_session.id.__str__()}})
+        vectorstore.delete_collection()
+        return response
+    else:
+        llm = ChatOpenAI(
+            temperature=0,
+            model_name="gpt-3.5-turbo",
+            openai_api_key=OPENAI_API_KEY
+        )
+        prompt = ChatPromptTemplate.from_template(
+            speak_to_teacher_chat_gpt_prompt_template)
+        rag_chain = (
+            {"context": RunnableLambda(lambda x: x['question']) | retriever | format_docs,
+             "question": RunnablePassthrough(),
+             "history": RunnableLambda(lambda x: x['history'])}
+            | prompt
+            | llm
+            | StrOutputParser()
+        )
+        conversational_rag_chain = RunnableWithMessageHistory(
+            rag_chain,
+            get_session_history,
+            input_messages_key="question",
+            history_messages_key="history",
+        )
+        response = conversational_rag_chain.invoke({"question": message_from_student}, config={
+            "configurable": {"session_id": interview_session.id.__str__()}})
+        vectorstore.delete_collection()
+        return response
 
 
 def extract_answer(interview_session: InterviewSessionModel, message_from_student: str, questions: Dict[int, InterviewQuestion]):
