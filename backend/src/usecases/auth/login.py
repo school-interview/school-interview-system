094b833e5ad524b574b184958469af69b2186fca
fujisaki <fujidan8464@gmail.com>
2024-09-22 02:51:46 +0900

    scattersいらなかった

--- backend/src/usecases/auth/login.py
+++ backend/src/usecases/auth/login.py
@@ -8,7 +8,7 @@ from typing import Tuple
 def login(db_session: Session, login_request: LoginRequest) -> UserModel:
     user_query: Tuple = select(UserModel).where(
         UserModel.student_id == login_request.student_id)
-    query_result = db_session.execute(user_query).scalars().first()
+    query_result = db_session.execute(user_query).first()
     user: UserModel = query_result[0] if query_result else None
     if user:
         user.name = login_request.name
