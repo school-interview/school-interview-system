11b7daa23271248c60f4049771893f203ab22017
fujisaki <fujidan8464@gmail.com>
2024-09-25 02:03:48 +0900

    InterivewRepositoryにspeakToTeacherを追加

--- client/lib/repository/interview/interview_repository.dart
+++ client/lib/repository/interview/interview_repository.dart
@@ -1,3 +1,7 @@
+import 'package:client/repository/api_result.dart';
+import 'package:openapi/api.dart';
 
-
-abstract class InterviewRepository {}
+abstract class InterviewRepository {
+  Future<ApiResult<TeacherResponse>> speakToTeacher(
+      String interviewSessionId, SpeakToTeacherRequest speakToTeacherRequest);
+}
