3ffcebc3f5aa8efd1a5bad9b1597293f341ea479
fujisaki <fujidan8464@gmail.com>
2024-09-27 00:21:09 +0900

    fix: /teachersエンドポイントが返すレスポンスはTeacherResponseに準拠するようにしました

--- backend/src/controllers/rest_api/teacher_rest_controller.py
+++ backend/src/controllers/rest_api/teacher_rest_controller.py
@@ -6,20 +6,20 @@ from fastapi import Depends
 from pydantic import TypeAdapter
 from pytest import Session
 from src.database import session_factory
-from src.models.db_models.teacher_model import Teacher, TeacherModel
-from src.models.rest_api_model import RestApiController
+from src.models import RestApiController, Teacher, TeacherModel, TeacherResponse
 
 
 class TeachersRestApiController(RestApiController):
     method = "GET"
     path = "/teachers"
-    response_model = List[Teacher]
+    response_model = TeacherResponse
 
     async def controller(self, db_session: Session = Depends(session_factory)):
         teacher_query = db_session.query(TeacherModel)
         teachers = [TypeAdapter(Teacher).validate_python(
             teacher[0].__dict__) for teacher in db_session.execute(teacher_query).all()]
-        return teachers
+        response = TeacherResponse(teachers=teachers, count=len(teachers))
+        return response
 
 
 teacher_rest_api_controllers: List[RestApiController] = [
