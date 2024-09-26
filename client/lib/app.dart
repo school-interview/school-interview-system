6bb29c3cdcbf6928730007960b9a60961bdeba11
Tsuri Yuma <yuma33t@gmail.com>
2024-09-26 17:45:22 +0900

    #98
    loggerを修正

--- client/lib/app.dart
+++ client/lib/app.dart
@@ -1,10 +1,4 @@
-import 'package:client/core/logger.dart';
 import 'package:logger/logger.dart';
 
 // ロガー
-final logger = Logger(
-  // ログ出力内容をシンプルにカスタマイズ
-  printer: ExtensionLogPrinter(),
-  // ログ出力したあとに処理を挿入する
-  output: ExtensionLogOutput(),
-);
+final logger = Logger();
