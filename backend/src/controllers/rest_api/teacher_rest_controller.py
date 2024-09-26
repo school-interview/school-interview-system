a6b15e22fc0941ef5e01b06cb69d73d8361539b0
fujisaki <fujidan8464@gmail.com>
2024-09-27 00:26:20 +0900

    TeacherResponse→TeachersResponse

--- backend/src/controllers/rest_api/teacher_rest_controller.py
+++ backend/src/controllers/rest_api/teacher_rest_controller.py
@@ -5,19 +5,19 @@ from sqlalchemy.orm import Session
 from fastapi import Depends
 from pydantic import TypeAdapter
 from src.database import session_factory
-from src.models import RestApiController, Teacher, TeacherModel, TeacherResponse
+from src.models import RestApiController, Teacher, TeacherModel, TeachersResponse
 
 
 class TeachersRestApiController(RestApiController):
     method = "GET"
     path = "/teachers"
-    response_model = TeacherResponse
+    response_model = TeachersResponse
 
     async def controller(self, db_session: Session = Depends(session_factory)):
         teacher_query = db_session.query(TeacherModel)
         teachers = [TypeAdapter(Teacher).validate_python(
             teacher[0].__dict__) for teacher in db_session.execute(teacher_query).all()]
-        response = TeacherResponse(teachers=teachers, count=len(teachers))
+        response = TeachersResponse(teachers=teachers, count=len(teachers))
         return response
 
 
