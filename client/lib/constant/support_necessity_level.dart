6aaefb1f726a8f623ccb14d7ad7d16a34ace2bc2
yuma.tsuri <yuma.tsuri@optim.co.jp>
2024-10-06 00:47:17 +0900

    #120 [要支援レベル画面] 要支援レベル表示修正
    正しい数値が表示されるように修正

--- client/lib/constant/support_necessity_level.dart
+++ client/lib/constant/support_necessity_level.dart
@@ -1,20 +1,64 @@
 import 'dart:ui';
 
-import 'package:client/constant/enum/support_necessity_level_element.dart';
-
 /// 要支援レベルの情報を取得するクラス
-class SupportNecessityLevel {
-  // タイトルとパラメータを取得する
-  (String, double, Color) getTitleAndParameter(SupportNecessityLevelElement supportNecessityLevelElement) {
-    switch (supportNecessityLevelElement) {
-      case SupportNecessityLevelElement.deviationFromMinimumAttendanceRate:
-        return ("出席率が66%を下回る", 32.5, const Color(0xFFff7675));
-      case SupportNecessityLevelElement.deviationFromPreferredCreditLevel:
-        return ("修得単位数が足りない", 27.5, const Color(0xFF74b9ff));
-      case SupportNecessityLevelElement.highAttendanceLowGpaRate:
-        return ("出席率が高く、GPAが低い", 22.5, const Color(0xFF55efc4));
-      case SupportNecessityLevelElement.lowAttendanceAndLowGpaRate:
-        return ("出席率が低く、GPAが低い", 17.5, const Color(0xFFffeaa7));
+class SupportLevel {
+  const SupportLevel({
+    required this.element,
+  });
+
+  final SupportLevelEnum element;
+
+  /// 各内訳要素の説明文を取得する
+  String get description {
+    switch (element) {
+      case SupportLevelEnum.attendanceRate:
+        return "出席率が66%を下回る";
+      case SupportLevelEnum.credit:
+        return "修得単位数が推奨数を下回る";
+      case SupportLevelEnum.highAttendanceLowGpa:
+        return "出席率が高く、GPAが低い";
+      case SupportLevelEnum.lowAttendanceAndLowGpa:
+        return "出席率が低く、GPAが低い";
     }
   }
+
+  /// 各内訳要素のパラメータを取得する
+  double get parameter {
+    switch (element) {
+      case SupportLevelEnum.attendanceRate:
+        return 32.5;
+      case SupportLevelEnum.credit:
+        return 27.5;
+      case SupportLevelEnum.highAttendanceLowGpa:
+        return 22.5;
+      case SupportLevelEnum.lowAttendanceAndLowGpa:
+        return 17.5;
+    }
+  }
+
+  /// 各内訳要素のグラフに用いるカラーを取得する
+  Color get chartColor {
+    switch (element) {
+      case SupportLevelEnum.attendanceRate:
+        return const Color(0xFFff7675);
+      case SupportLevelEnum.credit:
+        return const Color(0xFF74b9ff);
+      case SupportLevelEnum.highAttendanceLowGpa:
+        return const Color(0xFF55efc4);
+      case SupportLevelEnum.lowAttendanceAndLowGpa:
+        return const Color(0xFFffeaa7);
+    }
+  }
+}
+
+/// 要支援レベルの構成要素
+enum SupportLevelEnum {
+  // 出席率が低い
+  attendanceRate,
+  // 単位数が少ない
+  credit,
+  // 高出席率と低GPA
+  highAttendanceLowGpa,
+  // 低出席率と低GPA
+  lowAttendanceAndLowGpa,
 }
--- client/lib/notifier/interview_analytics_view/interview_analytics_view_notifier.dart
+++ client/lib/notifier/interview_analytics_view/interview_analytics_view_notifier.dart
@@ -10,5 +10,12 @@ class InterviewAnalyticsViewNotifier extends _$InterviewAnalyticsViewNotifier {
     return const InterviewAnalyticsViewState();
   }
 
-  void setIsAnimated(bool isAnimated) => state = state.copyWith(isAnimated: isAnimated);
+  void setIsAnimated(bool isAnimated) =>
+      state = state.copyWith(isAnimated: isAnimated);
+
+  /// 要支援レベルの内訳の値を生成する
+  String getElementValue(num? factor, double parameter) {
+    final value = ((factor?.toDouble() ?? 0) * parameter).toStringAsFixed(1);
+    return value;
+  }
 }
