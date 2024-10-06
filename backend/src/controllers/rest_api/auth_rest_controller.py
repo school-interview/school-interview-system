b5761793caefbc5e5c95e6b2300c20a498f0b379
fujisaki <fujidan8464@gmail.com>
2024-10-07 01:26:36 +0900

    verify部分&callback urlの変更

--- backend/src/controllers/rest_api/auth_rest_controller.py
+++ backend/src/controllers/rest_api/auth_rest_controller.py
@@ -3,13 +3,14 @@ from fastapi import Depends, HTTPException, Query
 from fastapi.security import OAuth2AuthorizationCodeBearer
 from fastapi.responses import RedirectResponse
 from src.database import session_factory
-from src.models import RestApiController, HttpMethod, LoginRequest, User
-from typing import List
+from src.models import RestApiController, ErrorResponse
+from typing import Any, List
 from src.usecases.auth.login import login
 from sqlalchemy.orm import Session
 from dotenv import load_dotenv
 import httpx
-
+from google.oauth2 import id_token
+from google.auth.transport import requests
 load_dotenv(".env.local")
 
 CLIENT_ID = os.getenv("CLIENT_ID")
@@ -22,6 +23,21 @@ oauth2_scheme = OAuth2AuthorizationCodeBearer(
     authorizationUrl=AUTHORIZATION_URL,
     tokenUrl=TOKEN_URL
 )
+
+
+async def verify_token(jwt: str):
+    try:
+        id_info = id_token.verify_oauth2_token(
+            jwt, requests.Request(), CLIENT_ID
+        )
+    except ValueError:
+        raise ErrorResponse(
+            status_code=400,
+            type="invalid_token",
+            title="Invalid token",
+            detail="The token provided is invalid."
+        )
+    return id_info
 
 
 class LoginRestApiController(RestApiController):
@@ -40,8 +56,8 @@ class LoginRestApiController(RestApiController):
 
 class OAuthCallbackRestApiController(RestApiController):
     method = "GET"
-    path = "/oauth2/login/callback"
-    response_model = None
+    path = "/oauth2/callback"
+    response_model = Any
 
     async def controller(self, code: str, session: Session = Depends(session_factory)):
         async with httpx.AsyncClient() as client:
@@ -54,8 +70,14 @@ class OAuthCallbackRestApiController(RestApiController):
             })
         token_response_json = token_response.json()
         if token_response.is_error:
-            raise HTTPException(status_code=400, detail=token_response_json)
-        return token_response_json
+            raise ErrorResponse(
+                status_code=400,
+                type="failed_to_get_token",
+                title="Failed to get token",
+                detail=token_response_json.get("error_description")
+            )
+        id_info = await verify_token(token_response_json["id_token"])
+        return id_info
 
 
 auth_rest_api_controllers: List[RestApiController] = [
