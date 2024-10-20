c232ccca623a7ca94d94f52e0ef3c8098b7110fd
fujisaki <fujidan8464@gmail.com>
2024-10-20 21:46:34 +0900

    studentな情報を更新するエンドポイント作成え

--- backend/src/controllers/rest_api/user_rest_controller.py
+++ backend/src/controllers/rest_api/user_rest_controller.py
@@ -2,11 +2,12 @@ from typing import List
 
 from fastapi import Depends
 from pydantic import TypeAdapter
+from sqlalchemy import UUID
 from sqlalchemy.orm import Session
-from src.controllers.rest_api.auth import verify_user
+from src.controllers.rest_api.auth import verify_admin, verify_user
 from src.database import session_factory
-from src.models.db_models.user_model import User, UserModel
-from src.models import RestApiController
+from src.models import RestApiController, User, UserModel, Student, StudentModel, StudentUpdate, ErrorResponse
+from src.crud import StudentsCrud
 
 
 class UsersRestApiController(RestApiController):
@@ -14,21 +15,32 @@ class UsersRestApiController(RestApiController):
     path = "/users"
     response_model = List[User]
 
-    async def controller(self, db_session: Session = Depends(session_factory), user_model=Depends(verify_user)):
+    async def controller(self, db_session: Session = Depends(session_factory), user_model=Depends(verify_admin)):
         user_query = db_session.query(UserModel)
         users = [TypeAdapter(User).validate_python(
             user[0].__dict__) for user in db_session.execute(user_query).all()]
         return users
 
 
-class MeRestApiController(RestApiController):
-    method = "GET"
-    path = "/me"
-    response_model = User
+class UpdateStudentRestApiController(RestApiController):
+    method = "PUT"
+    path = "/users/{user_id}/student"
+    response_model = Student
 
-    async def controller(self, user_model=Depends(verify_user)):
-        return TypeAdapter(User).validate_python(user_model.__dict__)
+    async def controller(self, setudent_update: StudentUpdate, user_id: str, db_session: Session = Depends(session_factory), user_model: UserModel = Depends(verify_user)):
+        if user_model.id.__str__() != user_id:
+            raise ErrorResponse(
+                status_code=400,
+                type="invalid_user_id",
+                title="Invalid user id",
+                detail="You can only update your own Student info."
+            )
+        student_crud = StudentsCrud(StudentModel)
+        student_model = student_crud.get_by_user_id(db_session, user_model.id)
+        student_model = student_crud.update(db_session, db_obj=student_model,
+                                            obj_in=setudent_update)
+        return student_model.convertToPydantic(Student)
 
 
 user_rest_api_controllers: List[RestApiController] = [
-    UsersRestApiController(), MeRestApiController()]
+    UsersRestApiController(), UpdateStudentRestApiController()]
--- backend/src/crud/students_crud.py
+++ backend/src/crud/students_crud.py
@@ -1,6 +1,9 @@
+from sqlalchemy import UUID
 from src.models import StudentModel, Student, StudentUpdate
 from src.crud.base_crud import BaseCrud
+from sqlalchemy.orm import Session
 
 
 class StudentsCrud(BaseCrud[StudentModel, Student, StudentUpdate]):
-    pass
+    def get_by_user_id(self, db_session: Session, user_id: UUID) -> StudentModel:
+        return db_session.query(StudentModel).filter(StudentModel.user_id == user_id).first()
