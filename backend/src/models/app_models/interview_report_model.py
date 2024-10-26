183ca4d64dd340558a4493cebf67d5b5c14a4dd0
fujisaki <fujidan8464@gmail.com>
2024-10-27 01:43:39 +0900

    InterviewReportModelの改名

--- backend/src/models/app_models/interview_report_model.py
+++ backend/src/models/app_models/interview_report_model.py
@@ -3,7 +3,8 @@ from src.models.db_models.user_model import UserModel
 from src.models.db_models.interview_analytics_model import InterviewAnalyticsModel
 from src.models.db_models.interview_session_model import InterviewSessionModel
 
-class InterviewReportModel(BaseModel):
-		user: UserModel
-		interview_session: InterviewSessionModel
-		analytics: InterviewAnalyticsModel
+
+class InterviewReport(BaseModel):
+    user: UserModel
+    interview_session: InterviewSessionModel
+    analytics: InterviewAnalyticsModel
