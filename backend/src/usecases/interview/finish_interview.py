3f047a245a533695a89582b3a1ee3db9d8c8810c
fujisaki <fujidan8464@gmail.com>
2024-10-27 18:06:22 +0900

    面談終了時にanalyticsテーブルに保存されるようにしました

--- backend/src/usecases/interview/finish_interview.py
+++ backend/src/usecases/interview/finish_interview.py
@@ -1,11 +1,14 @@
 
 from sqlalchemy.orm import Session
-from src.models import InterviewSessionModel
+from src.models import InterviewSessionModel, InterviewRecordModel
+from src.usecases.interview.analyze_interview import analyze_interview
 
 
-def finish_interview(session: Session, interview_session: InterviewSessionModel, chat_history_store: dict = {}):
+def finish_interview(db_session: Session, interview_session: InterviewSessionModel, *, chat_history_store: dict = {}, interview_record: InterviewRecordModel):
     interview_session.done = True
-    session.commit()
+    db_session.commit()
     store_key = interview_session.id.__str__()
+    if interview_record:
+        analyze_interview(db_session, interview_session, interview_record)
     if store_key in chat_history_store:
         del chat_history_store[store_key]
