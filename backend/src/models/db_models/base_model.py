99029318df976be428a1b8e5b6b2f3e181b10e9b
fujisaki <fujidan8464@gmail.com>
2024-10-27 14:10:40 +0900

    convertToPydanticで_obj_historyは最後にclearする＆循環参照検出ロジックにprimitiveな値は例外とするようにしました

--- backend/src/models/db_models/base_model.py
+++ backend/src/models/db_models/base_model.py
@@ -5,6 +5,8 @@ from pydantic.alias_generators import to_camel
 
 T = TypeVar('T')
 
+primitives = (bool, str, int, float, type(None))
+
 
 class EntityBaseModel(DeclarativeBase):
     def convertToPydantic(self, cls: Type[T], model_class_mapping: Dict[DeclarativeBase, BaseModel] = None, _obj_history=set()) -> Type[T]:
@@ -13,7 +15,8 @@ class EntityBaseModel(DeclarativeBase):
         keys = dict_to_convert.keys()
         for key in keys:
             value = dict_to_convert[key]
-            if id(value) in _obj_history:  # 以前に登場したオブジェクトはNone。（循環参照を防ぐため）
+            # 以前に登場したオブジェクトはNone。（循環参照を防ぐため）
+            if not (type(value) in primitives) and (id(value) in _obj_history):
                 dict_to_convert[key] = None
             elif issubclass(value.__class__, DeclarativeBase):
                 model: EntityBaseModel = dict_to_convert[key]
@@ -25,4 +28,5 @@ class EntityBaseModel(DeclarativeBase):
                     model_class, model_class_mapping, _obj_history)
                 dict_to_convert[key] = pydantic_model.__dict__
             _obj_history.add(id(value))
+        _obj_history.clear()
         return TypeAdapter(cls).validate_python(dict_to_convert)
