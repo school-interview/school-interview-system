32eaabc8d26c5aa1554c72fdb1fc5a88e83b7b66
fujisaki <fujidan8464@gmail.com>
2024-10-27 17:57:17 +0900

    InterviewAnalyticsModel.create_from_interview_recordにUserModelを渡していた（旧仕様）をStudentModelに変更

--- backend/src/models/error_models/not_student_exception_model.py
+++ backend/src/models/error_models/not_student_exception_model.py
@@ -1,5 +1,5 @@
 from src.models.error_models.app_exception_model import AppException
 
 
-class InterviewAlreadyStartedException(AppException):
+class NotStudentException(AppException):
     pass
--- backend/src/usecases/interview/analyze_interview.py
+++ backend/src/usecases/interview/analyze_interview.py
@@ -1,23 +1,25 @@
 from src.models import InterviewRecordModel
 from sqlalchemy.orm import Session
-from src.models import InterviewAnalyticsModel, UserModel, InterviewSessionModel
+from src.models import InterviewAnalyticsModel, UserModel, AdminModel, InterviewSessionModel, NotStudentException, UserNotFoundException
+from src.crud import InterviewAnalyticsCrud, UsersCrud, AdminCrud
 
 
 def analyze_interview(db_session: Session, interview_session: InterviewSessionModel, interview_recrod: InterviewRecordModel):
-    existing_interview_analytics_query = db_session.query(InterviewAnalyticsModel).where(
-        InterviewAnalyticsModel.session_id == interview_recrod.session_id)
-    query_result = db_session.execute(
-        existing_interview_analytics_query).first()
-    existing_interview_analytics = query_result[0] if query_result else None
+    analytics_crud = InterviewAnalyticsCrud(InterviewAnalyticsModel)
+    existing_interview_analytics = analytics_crud.get_by_session_id(
+        interview_session.id)
     if existing_interview_analytics:
         # すでにInterviewAnalyticsが存在している場合はそのまま返す。
         return existing_interview_analytics
-    user_query = db_session.query(UserModel).where(
-        UserModel.id == interview_session.user_id)
-    query_result = db_session.execute(user_query).first()
-    user = query_result[0] if query_result else None
+    users_crud = UsersCrud(UserModel)
+    user_model = users_crud.get(db_session, interview_session.user_id)
+    if not user_model:
+        raise UserNotFoundException("User not found.")
+    if user_model.is_admin:
+        raise NotStudentException("User is not a student.")
+    admin_crud = AdminCrud(AdminModel)
+    student_model = admin_crud.get_by_user_id(db_session, user_model.id)
     interview_analytics = InterviewAnalyticsModel.create_from_interview_record(
-        user, interview_recrod)
-    db_session.add(interview_analytics)
-    db_session.commit()
+        student_model, interview_recrod)
+    analytics_crud.create(db_session, interview_analytics)
     return interview_analytics
