1cabac9368a740c7868a278afc6b8b3d0e9c7cf3
fujisaki <fujidan8464@gmail.com>
2024-10-27 18:12:04 +0900

    コメント追加＆analyze_interviewのadmin取得処理をusers_crud.get_with_studentを使うことでクエリ数を1個減らしました

--- backend/src/usecases/interview/finish_interview.py
+++ backend/src/usecases/interview/finish_interview.py
@@ -5,6 +5,13 @@ from src.usecases.interview.analyze_interview import analyze_interview
 
 
 def finish_interview(db_session: Session, interview_session: InterviewSessionModel, *, chat_history_store: dict = {}, interview_record: InterviewRecordModel):
+    """
+        Args:
+            db_session (Session): DB session
+            interview_session (InterviewSessionModel): 面談セッション
+            chat_history_store (dict): 会話履歴の保存先
+            interview_record (InterviewRecordModel): 面談の記録。Noneの場合はanalyzeしない。(analyze結果はテーブルに保存される）
+    """
     interview_session.done = True
     db_session.commit()
     store_key = interview_session.id.__str__()
