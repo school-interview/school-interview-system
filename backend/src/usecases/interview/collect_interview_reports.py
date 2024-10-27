c2e1930c545914d11c3b8f7a9fdbecb8b76baff4
fujisaki <fujidan8464@gmail.com>
2024-10-27 11:10:46 +0900

    collect_interview_reportsの実装

---
+++ backend/src/usecases/interview/collect_interview_reports.py
@@ -0,0 +1,21 @@
+from typing import List
+from sqlalchemy import Row, Tuple
+from src.crud import InterviewAnalyticsCrud
+from src.models import InterviewAnalyticsModel, InterviewAnalytics, InterviewSessionModel, InterviewSession, UserModel, User, InterviewReport
+
+
+def collect_interview_reports():
+    analytics_crud = InterviewAnalyticsCrud(InterviewAnalyticsModel)
+    rows: List[Row[Tuple[InterviewAnalyticsModel, InterviewSessionModel,
+                         UserModel]]] = analytics_crud.get_multi_with_user_and_session()
+    interview_reports = list(
+        map(
+            lambda row: InterviewReport(
+                user=row[2].convertToPydantic(User),
+                interview_session=row[1].convertToPydantic(InterviewSession),
+                analytics=row[0].convertToPydantic(InterviewAnalytics)
+            ),
+            rows
+        )
+    )
+    return interview_reports
