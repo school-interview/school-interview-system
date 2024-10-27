c3140d7911002f79c842d89414806f56834ea5e0
fujisaki <fujidan8464@gmail.com>
2024-10-27 18:12:04 +0900

    コメント追加＆analyze_interviewのadmin取得処理をusers_crud.get_with_studentを使うことでクエリ数を1個減らしました

--- backend/src/usecases/interview/analyze_interview.py
+++ backend/src/usecases/interview/analyze_interview.py
@@ -1,10 +1,16 @@
 from src.models import InterviewRecordModel
 from sqlalchemy.orm import Session
-from src.models import InterviewAnalyticsModel, UserModel, AdminModel, InterviewSessionModel, NotStudentException, UserNotFoundException
-from src.crud import InterviewAnalyticsCrud, UsersCrud, AdminCrud
+from src.models import InterviewAnalyticsModel, UserModel, StudentModel, AdminModel, InterviewSessionModel, NotStudentException, UserNotFoundException
+from src.crud import InterviewAnalyticsCrud, UsersCrud, StudentsCrud
 
 
 def analyze_interview(db_session: Session, interview_session: InterviewSessionModel, interview_recrod: InterviewRecordModel):
+    """
+    Args:
+        db_session (Session): DB session
+        interview_session (InterviewSessionModel): 
+        interview_recrod (InterviewRecordModel):
+    """
     analytics_crud = InterviewAnalyticsCrud(InterviewAnalyticsModel)
     existing_interview_analytics = analytics_crud.get_by_session_id(
         interview_session.id)
@@ -12,14 +18,13 @@ def analyze_interview(db_session: Session, interview_session: InterviewSessionMo
         # すでにInterviewAnalyticsが存在している場合はそのまま返す。
         return existing_interview_analytics
     users_crud = UsersCrud(UserModel)
-    user_model = users_crud.get(db_session, interview_session.user_id)
+    user_model = users_crud.get_with_student(
+        db_session, interview_session.user_id)
     if not user_model:
         raise UserNotFoundException("User not found.")
     if user_model.is_admin:
         raise NotStudentException("User is not a student.")
-    admin_crud = AdminCrud(AdminModel)
-    student_model = admin_crud.get_by_user_id(db_session, user_model.id)
     interview_analytics = InterviewAnalyticsModel.create_from_interview_record(
-        student_model, interview_recrod)
+        user_model.student, interview_recrod)
     analytics_crud.create(db_session, interview_analytics)
     return interview_analytics
