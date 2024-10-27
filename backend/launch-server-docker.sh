bec7d85bf3521272141dcdff787a07e2bdc13b73
fujisaki <fujidan8464@gmail.com>
2024-10-27 16:21:38 +0900

    backend向けのデバッガーの設定

--- backend/launch-server-docker.sh
+++ backend/launch-server-docker.sh
@@ -2,4 +2,10 @@
 echo "Start migrating database..."
 poetry run alembic upgrade head
 echo "Database migrated successfully"
-poetry run uvicorn src.main:app_socketio --reload --host 0.0.0.0 --port 8000 
+if [ -n "$DEBUG" ] && [ "$DEBUG" = "True" ]; then
+	echo "Debug mode is on"
+	poetry run python -m debugpy --listen 0.0.0.0:5678 -m uvicorn src.main:app_socketio --reload --host '0.0.0.0' --port 8000
+else
+	echo "Debug mode is off"
+	poetry run uvicorn src.main:app_socketio --reload --host 0.0.0.0 --port 8000
+fi
