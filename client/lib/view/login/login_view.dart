670d23bfad8dc11d1c93cd4695a5769dee230dfa
yuma.tsuri <yuma.tsuri@optim.co.jp>
2024-11-02 23:20:49 +0900

    #126 [ログイン画面] ログイン画面を実装
    wip:ログイン処理

--- client/lib/view/login/login_view.dart
+++ client/lib/view/login/login_view.dart
@@ -2,10 +2,13 @@ import 'dart:html' as html;
 
 import 'package:client/component/custom_app_bar.dart';
 import 'package:client/constant/color.dart';
+import 'package:client/constant/result.dart';
 import 'package:client/constant/uri_string.dart';
 import 'package:client/notifier/login/login_notifier.dart';
+import 'package:client/router/go_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:sign_in_button/sign_in_button.dart';
 
 /// ログイン画面
@@ -17,10 +20,13 @@ class LoginView extends ConsumerStatefulWidget {
 }
 
 class _LoginView extends ConsumerState<LoginView> {
-  final formKey = GlobalKey<FormState>();
-
   @override
   Widget build(BuildContext context) {
+    ref.listen<Result?>(loginNotifierProvider.select((value) => value.result),
+        (_, next) {
+      // 処理結果ハンドリング
+      _handleResult(context, next);
+    });
     return Scaffold(
       backgroundColor: ColorDefinitions.primaryColor,
       appBar: CustomAppBar().startAppBar(context),
@@ -36,13 +42,13 @@ class _LoginView extends ConsumerState<LoginView> {
             ),
             onPressed: () {
               /// TODO iOSとAndroidでの実装
-              /// 現在の実装ではモバイルアプリでは動きません
+              /// モバイルアプリではポップアップが表示されません
               final loginNotifier = ref.read(loginNotifierProvider.notifier);
               html.window.open(UriString.googleLoginPageUri, "ログイン",
-                  'left=100,top=100,width=700,height=400');
+                  'left=100,top=100,width=700,height=500');
               html.window.onMessage.listen(
                 (html.MessageEvent event) async {
-                  await loginNotifier.getLoginToken();
+                  loginNotifier.login(event.data);
                 },
               );
             },
@@ -50,5 +56,30 @@ class _LoginView extends ConsumerState<LoginView> {
         ),
       ),
     );
+  }
+
+  /// 処理結果ハンドリング
+  ///
+  /// [result] 処理結果
+  void _handleResult(BuildContext context, Result? result) {
+    // final loginNotifier = ref.read(loginNotifierProvider.notifier);
+    final loginState = ref.watch(loginNotifierProvider);
+    switch (result) {
+      case Result.success:
+        // loginNotifier.setResult(null);
+        if (loginState.user?.isAdmin == true) {
+          // 教員向け画面へ遷移する
+          // TODO 教員向け画面を実装する
+        } else if (loginState.user?.isAdmin == false) {
+          // 学生向け画面へ遷移する
+          context.push(RouterPath.profileInputView);
+        }
+        break;
+      case Result.fail:
+        // TODO API処理に失敗した際の処理を追加する（アラートを表示→再度API実行など）
+        break;
+      default:
+        break;
+    }
   }
 }
