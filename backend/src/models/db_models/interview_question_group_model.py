3a058a406ab499f97186977d81d341960509e515
fujisaki <fujidan8464@gmail.com>
2024-11-20 00:13:27 +0900

    interview_questionとinterview_question_groupのデータモデル変更

--- backend/src/models/db_models/interview_question_group_model.py
+++ backend/src/models/db_models/interview_question_group_model.py
@@ -1,8 +1,10 @@
 from uuid import UUID
 from typing import Any, List, Optional
+from sqlalchemy import String
 from src.models.db_models.base_model import EntityBaseModel
 from src.models.app_pydantic_base_model import AppPydanticBaseModel
 from src.models.db_models.interview_question_model import InterviewQuestion, InterviewQuestionModel
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 
 class InterviewQuestionGroup(AppPydanticBaseModel):
@@ -10,3 +12,10 @@ class InterviewQuestionGroup(AppPydanticBaseModel):
     group_name: str
     order: int
     questions: List[InterviewQuestion]
+
+
+class InterviewQuestionGroupModel(EntityBaseModel):
+    __tablename__ = "InterviewQuestionGroups"
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    group_name: Mapped[str] = mapped_column(String(100))
+    questions: Mapped[List[InterviewQuestionModel]] = relationship()
--- backend/src/models/db_models/interview_question_model.py
+++ backend/src/models/db_models/interview_question_model.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 from src.models.db_models.base_model import EntityBaseModel
-from typing import Any, Optional
+from typing import Any, Literal, Optional
 from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import mapped_column, Mapped
 from src.models.app_pydantic_base_model import AppPydanticBaseModel
@@ -11,10 +11,13 @@ class InterviewQuestion(AppPydanticBaseModel):
     question: str
     order: int
     group_id: UUID
-    condition_value: Optional[str]
-    condition_operator: Optional[str]
+    left_operand: Optional[str]
+    left_operator: Optional[Literal['==', '>', '<', '>=', '<=', '!=']]
+    right_operand: Optional[str]
+    right_operator: Optional[Literal['==', '>', '<', '>=', '<=', '!=']]
     prompt: str
     description: Optional[str]
+    extraction_data_type: Literal['int', 'float', 'str', 'bool']
 
 
 class InterviewQuestionModel(EntityBaseModel):
@@ -24,10 +27,13 @@ class InterviewQuestionModel(EntityBaseModel):
         ForeignKey("InterviewQuestionGroup.id", ondelete="CASCADE"))
     question: Mapped[str] = mapped_column(String(100))
     order: Mapped[int] = mapped_column(unique=True)
-    condition_value: Mapped[Optional[str]] = mapped_column(String(20))
-    condition_operator: Mapped[Optional[str]] = mapped_column(String(2))
+    left_operand: Mapped[Optional[str]] = mapped_column(String(10))
+    left_operator: Mapped[Optional[str]] = mapped_column(String(2))
+    right_operand: Mapped[Optional[str]] = mapped_column(String(10))
+    right_operator: Mapped[Optional[str]] = mapped_column(String(2))
     prompt: Mapped[str] = mapped_column(String(200))
     description: Mapped[Optional[str]] = mapped_column(String(200))
+    extraction_data_type: Mapped[str] = mapped_column(String(5))
 
 
 class InterviewQuestionUpdate(AppPydanticBaseModel):
