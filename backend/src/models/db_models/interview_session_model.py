67f8b42c7da2fc8fe1d35df6108690b1905c87ae
fujisaki <fujidan8464@gmail.com>
2024-11-21 22:26:26 +0900

    InterviewSessionとInterviewQuestionのリレーション

--- backend/src/models/db_models/interview_session_model.py
+++ backend/src/models/db_models/interview_session_model.py
@@ -1,11 +1,14 @@
 from uuid import UUID
-from src.models import EntityBaseModel, User, Teacher
 from sqlalchemy import String, ForeignKey
 from typing import List, Optional
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy import DateTime
 from datetime import datetime
 from pydantic import BaseModel, Field
+from src.models.db_models.base_model import EntityBaseModel
+from src.models.db_models.interview_question_model import InterviewQuestion, InterviewQuestionModel
+from src.models.db_models.teacher_model import Teacher
+from src.models.db_models.user_model import User
 from src.models.app_pydantic_base_model import AppPydanticBaseModel
 
 
@@ -16,7 +19,8 @@ class InterviewSession(AppPydanticBaseModel):
     teacher_id: UUID
     teacher: Optional[Teacher] = Field(None)
     start_at: datetime
-    progress: int
+    current_question_id: UUID
+    current_question: Optional[InterviewQuestion] = Field(None)
     done: bool
 
 
@@ -30,8 +34,10 @@ class InterviewSessionModel(EntityBaseModel):
     teacher_id: Mapped[UUID] = mapped_column(ForeignKey("Teachers.id"))
     teacher = relationship("TeacherModel", backref="interview_sessions")
     start_at: Mapped[datetime] = mapped_column(type_=DateTime(timezone=True))
-    progress: Mapped[int] = mapped_column(
-        ForeignKey("InterviewQuestions.order"))
+    current_question_id: Mapped[UUID] = mapped_column(
+        ForeignKey("InterviewQuestions.id"))
+    current_question: Mapped[Optional[InterviewQuestionModel]] = relationship(
+        "InterviewQuestionModel", back_populates="interview_sessions")
     done: Mapped[bool]
 
 
