62f3ab96d2fa3016a24e944ae0c5ca2d7e4cf1ad
fujisaki <fujidan8464@gmail.com>
2024-11-21 23:06:50 +0900

    interview_sessions_crudにget_with_current_question_by_user_idを実装

--- backend/src/crud/interview_sessions_crud.py
+++ backend/src/crud/interview_sessions_crud.py
@@ -1,6 +1,16 @@
+from uuid import UUID
 from src.crud.base_crud import BaseCrud
 from src.models import InterviewSessionModel, InterviewSession, InterviewSessionUpdate
+from sqlalchemy.orm import Session, joinedload
 
 
 class InterviewSessionsCrud(BaseCrud[InterviewSessionModel, InterviewSession, InterviewSessionUpdate]):
-    pass
+    def get_with_curernt_question_by_user_id(self, db_session: Session, user_id: UUID):
+        return db_session.query(
+            InterviewSessionModel
+        ).options(
+            joinedload(InterviewSessionModel.current_question,
+                       innerjoin=False)
+        ).filter(
+            InterviewSessionModel.user_id == user_id
+        ).first()
