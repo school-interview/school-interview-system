ece7c57c382e03b0eba47e5b46ad4f2b3e34884b
fujisaki <fujidan8464@gmail.com>
2024-11-24 21:09:51 +0900

    WIP: CRUDのテスト実装中（sqliteを用いたテストでリレーションが失敗する）

--- backend/src/crud/interview_sessions_crud.py
+++ backend/src/crud/interview_sessions_crud.py
@@ -2,15 +2,16 @@ from uuid import UUID
 from src.crud.base_crud import BaseCrud
 from src.models import InterviewSessionModel, InterviewSession, InterviewSessionUpdate
 from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.dialects import sqlite
 
 
 class InterviewSessionsCrud(BaseCrud[InterviewSessionModel, InterviewSession, InterviewSessionUpdate]):
     def get_with_curernt_question_by_user_id(self, db_session: Session, user_id: UUID):
-        return db_session.query(
+        query = db_session.query(
             InterviewSessionModel
         ).options(
-            joinedload(InterviewSessionModel.current_question,
-                       innerjoin=False)
+            joinedload(InterviewSessionModel.current_question, innerjoin=True)
         ).filter(
             InterviewSessionModel.user_id == user_id
-        ).first()
+        )
+        return query.first()
