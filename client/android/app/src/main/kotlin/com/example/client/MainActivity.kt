1b83906a1a1449d7f6f00375102fa38a57045790
Tsuri Yuma <yuma33t@gmail.com>
2024-12-12 17:27:14 +0900

    Googleフォーム修正

--- client/android/app/src/main/kotlin/com/example/client/MainActivity.kt
+++ client/android/app/src/main/kotlin/com/example/client/MainActivity.kt
@@ -2,4 +2,11 @@ package com.example.client
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+import io.flutter.plugins.urllauncher.UrlLauncherPlugin
+
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        UrlLauncherPlugin.registerWith(registrarFor("io.flutter.plugins.urllauncher.UrlLauncherPlugin"))
+    }
+}
